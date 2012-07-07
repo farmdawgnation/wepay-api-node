e0b7f4a036c2811c1669734d43a79cf70d80b152
Matt Farmer <matt@frmr.me>
2012-07-07 13:10:36 -0400

    Remove the modularized test files.
    Our tests aren't really that complex and we need some ordering control.

--- test/Checkout.js
+++
@@ -1,6 +0,0 @@
-/* WePay API for Node.js
- * (c)2012 Matt Farmer
- * Release without warranty under the terms of the
- * Apache License. For more details, see the LICENSE
- * file at the root of this project.
-*/
