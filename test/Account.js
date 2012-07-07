61b2bc61347fb89fb297fa036986001a01fef8b6
Matt Farmer <matt@frmr.me>
2012-07-07 01:13:21 -0400

    Add vows dependancy; add placeholder test files.

---
+++ test/Account.js
@@ -0,0 +1,6 @@
+/* WePay API for Node.js
+ * (c)2012 Matt Farmer
+ * Release without warranty under the terms of the
+ * Apache License. For more details, see the LICENSE
+ * file at the root of this project.
+*/
